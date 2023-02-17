036ac3780f962904d25fd46a6c8dc36eeebb2d69
Tadas <vtadas25@gmail.com>
2023-02-17 11:48:31 +0200

    Remove SMS based authentication
    Responding to telegram's API changes.

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -8,11 +8,9 @@ import '../../global/actions/initial';
 import { pick } from '../../util/iteratees';
 import { PLATFORM_ENV } from '../../util/environment';
 import windowSize from '../../util/windowSize';
-import useHistoryBack from '../../hooks/useHistoryBack';
 import useCurrentOrPrev from '../../hooks/useCurrentOrPrev';
 
 import Transition from '../ui/Transition';
-import AuthPhoneNumber from './AuthPhoneNumber';
 import AuthCode from './AuthCode.async';
 import AuthPassword from './AuthPassword.async';
 import AuthRegister from './AuthRegister.async';
@@ -30,7 +28,7 @@ const Auth: FC<OwnProps & StateProps> = ({
   isActive, authState, hasWebAuthTokenPasswordRequired,
 }) => {
   const {
-    reset, initApi, returnToAuthPhoneNumber, goToAuthQrCode,
+    reset, initApi,
   } = getActions();
 
   useEffect(() => {
@@ -41,20 +39,6 @@ const Auth: FC<OwnProps & StateProps> = ({
   }, [isActive, reset, initApi, hasWebAuthTokenPasswordRequired]);
 
   const isMobile = PLATFORM_ENV === 'iOS' || PLATFORM_ENV === 'Android';
-
-  const handleChangeAuthorizationMethod = () => {
-    if (!isMobile) {
-      goToAuthQrCode();
-    } else {
-      returnToAuthPhoneNumber();
-    }
-  };
-
-  useHistoryBack({
-    isActive: (!isMobile && authState === 'authorizationStateWaitPhoneNumber')
-      || (isMobile && authState === 'authorizationStateWaitQrCode'),
-    onBack: handleChangeAuthorizationMethod,
-  });
 
   // Prevent refresh when rotating device
   useEffect(() => {
@@ -79,12 +63,10 @@ const Auth: FC<OwnProps & StateProps> = ({
         return <AuthPassword />;
       case 'authorizationStateWaitRegistration':
         return <AuthRegister />;
-      case 'authorizationStateWaitPhoneNumber':
-        return <AuthPhoneNumber />;
       case 'authorizationStateWaitQrCode':
         return <AuthQrCode />;
       default:
-        return isMobile ? <AuthPhoneNumber /> : <AuthQrCode />;
+        return <AuthQrCode />;
     }
   }
 
